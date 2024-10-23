ac557aeb71c6ad539a6cb4910519d3556369b672
Vishal Singh <vishalsing384@gmail.com>
2024-10-23 20:52:51 +0530

    :bug::lipstick: dist folder and eco config

--- server/src/index.ts
+++ server/src/index.ts
@@ -32,7 +32,7 @@ app.use("/*",(req,res)=>{
 })
 
 /* SERVER */
-const port=process.env.PORT||3001;
-app.listen(port, ()=>{
+const port=Number(process.env.PORT)||3001;
+app.listen(port,"0.0.0.0", ()=>{
     console.log(`Server running on port ${port}`);
 })
